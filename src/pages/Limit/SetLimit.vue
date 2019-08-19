3a85303562c4923a9ebc958efc3194d7532a2d6b
hss <hss@qq.com>
2019-08-19 18:37:37 +0800

    子后台

--- src/pages/Limit/SetLimit.vue
+++ src/pages/Limit/SetLimit.vue
@@ -1,14 +1,236 @@
 <template>
-<div class="setlimit">
-    我是权限设置
-</div>
-
+  <div id="setlimit">
+    <div class="inp-area">
+      <div class="row">
+        <div class="label">角色名称</div>
+        <div class="input">
+          <el-input size="medium" v-model="user_name" placeholder="请输入角色名"></el-input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="label">角色描述</div>
+        <div class="input">
+          <el-input size="medium" v-model="user_describe" placeholder="请输入角色名"></el-input>
+        </div>
+      </div>
+      <div class="row">
+        <div class="label">授权管理</div>
+      </div>
+    </div>
+    <div class="permission-bd">
+      <el-table
+        :data="permission"
+        row-key="menu"
+        :tree-props="{children: 'children', hasChildren: 'hasChildren'}"
+      >
+        <el-table-column
+          v-for="(item, index) in tableStyle"
+          :prop="item.prop"
+          :label="item.label"
+          :key="index"
+        >
+          <template slot-scope="scope">
+            <template v-if="index !== 1">{{scope.row[item.prop]}}</template>
+            <template v-if="index === 1">
+              <el-checkbox-group v-model="checkList[scope.row[item.prop].key]">
+                <el-checkbox
+                  v-for="it in scope.row[item.prop].list"
+                  :label="it.name"
+                  :key="it.name"
+                  @change="singleCheckboxChange(
+                  checkList[scope.row[item.prop].key].indexOf(it.name) >= 0,
+                  it.name,
+                  scope.row[item.prop].key
+                )"
+                >{{it.text}}</el-checkbox>
+              </el-checkbox-group>
+            </template>
+          </template>
+        </el-table-column>
+      </el-table>
+       <div class="t-c" style="margin-top:20px">
+        <el-button type="info" size="medium" @click="back()">取消</el-button>
+        <permission-button :action="ActionType.EDIT">
+          <el-button type="primary" size="medium">提交</el-button>
+        </permission-button>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
+import PermissionButton from "../../plugin/components/PermissionButton";
+import BaseIframe from "../../plugin/script/common/BaseIframe";
 export default {
+  components: { PermissionButton },
+  extends: BaseIframe,
+  data() {
+    return {
+      user_name: "",
+      user_describe: "",
+      permission: [],
+      checkList: {},
+      tableStyle: [
+        { label: "菜单", prop: "menu", width: "" },
+        { label: "全选", prop: "model", width: "" }
+      ]
+    };
+  },
+  mounted() {
+    this.init();
+  },
+  methods: {
+    init() {
+      /** 测试数据 **/
+      let examplePermission = [
+        {
+          menu: "账户管理",
+          model: {
+            key: "user",
+            list: [new PermissionCheckbox("all", "全部", 1)]
+          },
+          children: [
+            {
+              menu: "充值记录",
+              model: {
+                key: "depositRecords",
+                list: [
+                  new PermissionCheckbox("all", "全部", 1),
+                  new PermissionCheckbox("read", "查看", 1)
+                ]
+              }
+            },
+            {
+              menu: "登陆日志",
+              model: {
+                key: "loginRecords",
+                list: [
+                  new PermissionCheckbox("all", "全部", 1),
+                  new PermissionCheckbox("read", "查看", 1)
+                ]
+              }
+            }
+          ]
+        },
+        {
+          menu: "权限管理",
+          model: {
+            key: "permission",
+            list: [new PermissionCheckbox("all", "全部", 1)]
+          },
+          children: [
+            {
+              menu: "角色列表",
+              model: {
+                key: "userList",
+                list: [
+                  new PermissionCheckbox("all", "全部", 1),
+                  new PermissionCheckbox("read", "查看", 1)
+                ]
+              }
+            },
+            {
+              menu: "账户列表",
+              model: {
+                key: "accountList",
+                list: [
+                  new PermissionCheckbox("all", "全部", 1),
+                  new PermissionCheckbox("read", "查看", 1)
+                ]
+              }
+            }
+          ]
+        }
+      ];
 
-}
+      this.permission = examplePermission;
+      this.checkList = (function() {
+        let selectLs = ls => {
+          let obj = {};
+          for (let i = 0; i < ls.length; i++) {
+            let item = ls[i];
+            obj[item.model.key] = (function() {
+              let ls = [];
+              for (let j = 0; j < item.model.list.length; j++) {
+                let it = item.model.list[j];
+                if (it.val === 1) {
+                  ls.push(it.name);
+                }
+              }
+              return ls;
+            })();
+
+            if (item.children && item.children.length > 0)
+              obj = { ...obj, ...selectLs(item.children) };
+          }
+          return obj;
+        };
+        return selectLs(examplePermission);
+      })();
+      console.log(this.modelPath);
+      console.log(this.checkList);
+    },
+    // TODO 硬编码
+    singleCheckboxChange(val, name, model) {
+      console.log(val, name, model);
+
+      /** 点击全部 **/
+      if (name === "all") {
+        if (this.modelPath.hasOwnProperty(model)) {
+          for (let i = 0; i < this.modelPath.length; i++) {
+            /**  **/
+          }
+        }
+      }
+    }
+  },
+    computed: {
+      modelPath() {
+        let obj = {};
+        for (let i = 0; i < this.permission.length; i++) {
+          let permissionObj = this.permission[i];
+          let item = {};
+          let name = permissionObj.model['key'];
+          item = ((ls) => {
+            let l = [];
+            for (let j = 0; j < ls.length; j++) {
+              l.push(ls[j].model['key']);
+            }
+            return l;
+          })(permissionObj.children || []);
+          obj[name] = item;
+        }
+        return obj;
+      },
+    },
+    
+};
+ class PermissionCheckbox {
+    constructor(name, text, val) {
+      this.name = name;
+      this.text = text;
+      this.val = val;
+    }
+  }
 </script>
 <style scoped>
-
+#setlimit {
+}
+#setlimit .inp-area {
+  padding-left: 60px;
+}
+#setlimit .inp-area .row {
+  display: flex;
+  align-items: center;
+  justify-content: left;
+  padding: 15px 0;
+}
+#setlimit .inp-area .row .label {
+  padding-right: 30px;
+}
+#setlimit .inp-area .row .el-input {
+  width: 450px;
+}
+#setlimit .inp-area .row .el-input input {
+  border-radius: 0;
+}
 </style>
--- src/pages/SubBackstage/Subaccount.vue
+++ src/pages/SubBackstage/Subaccount.vue
@@ -1,14 +1,147 @@
 <template>
-<div class="subaccount">
-    我是子后台账号列表
-</div>
-
+  <div id="subaccount">
+    <div class="input-area">
+      <el-input v-model="sub_id" placeholder="子后台ID" size="medium"></el-input>
+      <el-input v-model="sub_name" placeholder="子后台名称" size="medium"></el-input>
+      <permission-button :action="ActionType.READ" @click="search()">
+        <el-button type="primary" size="medium">查询</el-button>
+      </permission-button>
+      <permission-button :action="ActionType.ADD" @click="addsub=true">
+        <el-button type="primary" size="medium">新增</el-button>
+      </permission-button>
+    </div>
+    <div class="bd">
+      <info-table
+        :search="search"
+        :table-style="tableStyle"
+        :records="records"
+        :page-info="pageInfo"
+      ></info-table>
+    </div>
+    <el-dialog :visible.sync="addsub" width="50%" title="新增子后台账号">
+      <div class="checkbox">
+        <el-form
+          :model="ruleForm"
+          :rules="rules"
+          ref="ruleForm"
+          label-width="100px"
+          class="demo-ruleForm"
+        >
+          <p>
+            <el-form-item label="子后台名称" prop="substage" class="formleft">
+              <el-input v-model="ruleForm.substage" placeholder="请输入名称"></el-input>
+            </el-form-item>
+            <el-form-item label="子后台账号" prop="subaccount">
+              <el-input v-model="ruleForm.subaccount" placeholder="请输入昵称"></el-input>
+            </el-form-item>
+          </p>
+          <p>
+            <el-form-item label="密码" prop="password" class="formleft">
+              <el-input v-model.number="ruleForm.password" placeholder="（6-8位数字）"></el-input>
+            </el-form-item>
+            <el-form-item label="所属角色" prop="belongrole">
+              <el-select v-model="ruleForm.belongrole" placeholder="用户角色">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+          </p>
+          <p>
+            <el-form-item label="子后台描述" prop="silverdescribe">
+              <el-input type="textarea" v-model="ruleForm.silverdescribe" placeholder="请输入描述"></el-input>
+            </el-form-item>
+          </p>
+        </el-form>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addsub = false" class="cancel">取 消</el-button>
+        <el-button type="primary" @click="addsub = false,submitForm('ruleForm')" class="confirm">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
+
 <script>
+import PermissionButton from "../../plugin/components/PermissionButton";
+import BaseIframe from "../../plugin/script/common/BaseIframe";
+import InfoTable from "../../plugin/components/InfoTable";
+import PageInfo from "../../plugin/script/common/PageInfo";
+
 export default {
-
-}
+  extends: BaseIframe,
+  components: { InfoTable, PermissionButton },
+  data() {
+       var validatePass = (rule, value, callback) => {
+      if (!Number.isInteger(value)) {
+        return callback(new Error("密码为纯数字"));
+      } else if (value.toString().length < 6 || value.toString().length > 8) {
+        return callback(new Error("密码位数为6-8位"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      sub_id: "",
+      sub_name: "",
+      date: [],
+      addsub: false,
+      ruleForm: {
+        substage: "",
+        subaccount:'',
+        password: "",
+        belongrole: "",
+      },
+      rules: {
+        substage: [{ required: true, message: "请输入用户名", trigger: "blur" }],
+        subaccount: [{ required: true, message: "请输入昵称", trigger: "blur" }],
+        password: [{ required: true, message: "请输入密码", trigger: "blur" },
+          { validator: validatePass, trigger: "blur" }],
+        belongrole: [
+          { required: true, message: "请选择角色", trigger: "change" }
+        ]
+      },
+      tableStyle: [
+        { label: "子后台ID", prop: "substageid", width: "" },
+        { label: "子后台名称", prop: "substagename", width: "" },
+        { label: "子后台账号", prop: "substageaccount", width: "" },
+        { label: "下级后台数", prop: "lowerstage", width: "" },
+        { label: "用户类型", prop: "usertype", width: "" },
+        { label: "子后台描述", prop: "substagedescribe", width: "" },
+        { label: "操作", prop: "operate", width: "" },
+      ],
+      records: [],
+      pageInfo: new PageInfo(0, [10, 15, 20], 0)
+    };
+  },
+  methods: {
+    search() {}
+  }
+};
 </script>
-<style scoped>
 
+<style scoped>
+.el-input {
+  margin-right: 10px;
+}
+.formleft {
+  margin-right: 100px;
+}
+p {
+  margin-bottom: 30px;
+}
+.el-textarea {
+  width: 630px;
+}
+.cancel,
+.confirm {
+  width: 160px !important;
+  margin: 0 100px;
+  color: #fff !important;
+  font-size: 15px !important;
+  font-weight: 650 !important;
+}
+.cancel {
+  background-color: #ccc;
+  border: transparent;
+}
 </style>
